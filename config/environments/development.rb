c051a8b8802f452669bae57d3867ea008a12098e
jofel <fodor.jofel@gmail.com>
2016-11-14 15:49:23 +0100

    m

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,7 +54,7 @@ Rails.application.configure do
 
 
   #config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.action_mailer.default_url_options = { :host => 'hermannote.heroku.com' }
+  config.action_mailer.default_url_options = { :host => 'hermannote.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   # config.action_mailer.smtp_settings = {    :address => "smtp.gmail.com",
